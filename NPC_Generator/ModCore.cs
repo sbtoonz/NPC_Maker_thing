9f923f39bd2b366d3df0ca13214f26e6c4dec32d
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-03-05 14:03:04 -0700

    Yay sync on load/reload/relog/other playrs can see now too

--- NPC_Generator/ModCore.cs
+++ NPC_Generator/ModCore.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using BepInEx;
 using HarmonyLib;
-using NPC_Generator.NPC_Classes;
 using UnityEngine;
 
 namespace NPC_Generator
@@ -10,11 +9,10 @@ namespace NPC_Generator
     [BepInPlugin(ModGUID, ModName, ModVersion)]
     public class NPC_Generator : BaseUnityPlugin
     {
-        private const string ModName = "New Mod";
+        private const string ModName = "NPC_Maker";
         private const string ModVersion = "1.0";
-        private const string ModGUID = "some.new.guid";
+        private const string ModGUID = "com.odinplus.NPC_Maker";
         private static Harmony harmony = null!;
-        internal static GameObject? npc_prefab;
         internal static GameObject? RootGOHolder;
         internal static GameObject? NetworkedNPC;
         
@@ -28,46 +26,89 @@ namespace NPC_Generator
             RootGOHolder.SetActive(false);
         }
 
-        [HarmonyPatch(typeof(Game), nameof(Game.Awake))]
-        public static class GamestartPatch
+        [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.Start))]
+        public static class TestPatch
         {
-            public static void Postfix(Game __instance)
+            public static void Postfix(FejdStartup __instance)
             {
-                NpcManager.SetupNPC();
-                NpcManager.Init();
+                BuildHumanNPC();
             }
         }
 
         [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
-        [HarmonyPriority(Priority.Last)]
-        public static class ZNSAwakepatch
+        public static class ZNSAwakePatch
         {
             public static void Prefix(ZNetScene __instance)
             {
-                if (__instance.m_prefabs.Count <= 0) return;
+                if(__instance.m_prefabs.Count <= 0) return;
+                __instance.m_prefabs.Add(NetworkedNPC);
                 
-                if(NetworkedNPC != null) __instance.m_prefabs.Add(NetworkedNPC);
-             }
-
-            public static void Postfix(ZNetScene __instance)
-            {
-                NpcManager.ZNSStuff();
-               var test = __instance.m_prefabs.Find(x => x.name == "BasicHuman");
-               __instance.m_prefabs.Remove(test);
-               __instance.m_prefabs.Add(NetworkedNPC);
             }
         }
 
-        [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.OnDestroy))]
-        public static class onZnetDestroy
+        private static void BuildHumanNPC()
         {
-            public static void Postfix()
+            var temp = Resources.FindObjectsOfTypeAll<GameObject>();
+            GameObject? tempplayer = null;
+            foreach (var o in temp)
             {
-                foreach (Transform VARIABLE in RootGOHolder.transform)
-                {
-                    Destroy(VARIABLE.gameObject);
+                if (o.GetComponent<Player>() != null)
+                { 
+                    NetworkedNPC = Instantiate(o, RootGOHolder?.transform!);
+                    tempplayer = o;
+                    break;
                 }
             }
+            DestroyImmediate(NetworkedNPC?.GetComponent<PlayerController>());
+            DestroyImmediate(NetworkedNPC?.GetComponent<Player>());
+            DestroyImmediate(NetworkedNPC?.GetComponent<Talker>());
+            DestroyImmediate(NetworkedNPC?.GetComponent<Skills>());
+            NetworkedNPC!.name = "Basic Human";
+            var basicznet =
+                NetworkedNPC.GetComponent<ZNetView>();
+            basicznet.enabled = true;
+            NetworkedNPC.GetComponent<ZSyncAnimation>().enabled = true;
+            NetworkedNPC.GetComponent<ZSyncTransform>().enabled = true;
+            var HumanoidAI = NetworkedNPC.AddComponent<Humanoid>();
+            HumanoidAI.m_nview = basicznet;
+            basicznet.m_persistent = true;
+            HumanoidAI.CopyChildrenComponents<Humanoid, Player>(tempplayer!.GetComponent<Player>());
+            NetworkedNPC.AddComponent<Tameable>();
+            NetworkedNPC.name = "BasicHuman";
+            NetworkedNPC.transform.name = "BasicHuman";
+            NetworkedNPC.transform.position = Vector3.zero;
+            //go.AddComponent<NPC_Human>();
+            var MonsterAI =
+                NetworkedNPC.AddComponent<MonsterAI>();
+            SetupMonsterAI(MonsterAI);
+            MonsterAI.m_nview = NetworkedNPC.GetComponent<ZNetView>();
+            MonsterAI.m_nview.m_zdo = new ZDO();
+            var hum = NetworkedNPC.GetComponent<Humanoid>();
+            hum.m_faction = Character.Faction.PlainsMonsters;
+            hum.m_health = 200;
+            hum.m_defaultItems = new GameObject[0];
+            hum.m_eye = NetworkedNPC.transform.Find("EyePos");
+        }
+        private static void SetupMonsterAI(MonsterAI ai)
+        {
+            ai.m_viewRange = 30;
+            ai.m_viewAngle = 90;
+            ai.m_hearRange = 9999;
+            ai.m_alertedEffects.m_effectPrefabs = new EffectList.EffectData[0];
+            ai.m_idleSound.m_effectPrefabs = new EffectList.EffectData[0];
+            ai.m_pathAgentType = Pathfinding.AgentType.Humanoid;
+            ai.m_smoothMovement = true;
+            ai.m_jumpInterval = 20;
+            ai.m_randomCircleInterval = 2;
+            ai.m_randomMoveInterval = 30;
+            ai.m_randomMoveRange = 3;
+            ai.m_alertRange = 20;
+            ai.m_fleeIfHurtWhenTargetCantBeReached = true;
+            ai.m_fleeIfLowHealth = 0;
+            ai.m_circulateWhileCharging = true;
+            ai.m_enableHuntPlayer = true;
+            ai.m_wakeupRange = 5;
+            ai.m_wakeupEffects.m_effectPrefabs = new EffectList.EffectData[0];
         }
         public void OnDestroy()
         {
