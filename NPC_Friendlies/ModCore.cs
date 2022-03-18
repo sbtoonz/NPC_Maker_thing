76f9d2ec41c12682f9957f76f5621e093a8939b7
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-03-17 20:51:22 -0600

    ServerSync this

--- NPC_Friendlies/ModCore.cs
+++ NPC_Friendlies/ModCore.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using ServerSync;
 
 namespace NPC_Friendlies
 {
@@ -18,6 +19,7 @@ namespace NPC_Friendlies
         private static Harmony harmony = null!;
 
         //
+        internal static ConfigEntry<bool>? _serverConfigLocked;
         public static ConfigEntry<int> FeedDuration;
         public static ConfigEntry<string> BrutePrefabName;
         public static ConfigEntry<string> TamingItemList;
@@ -33,6 +35,20 @@ namespace NPC_Friendlies
         public static ConfigEntry<int> Agressiveness;
         public static ConfigEntry<int> Mobility;
         public static ConfigEntry<int> Intelligence;
+        private static ConfigSync configSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion};
+
+        ConfigEntry<T> config<T>(string group, string configName, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, configName, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string configName, T value, string description, bool synchronizedSetting = true) => config(group, configName, value, new ConfigDescription(description), synchronizedSetting);
+
         public void Awake()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
@@ -43,22 +59,23 @@ namespace NPC_Friendlies
 
         internal void SetupConfigs()
         {
-            BrutePrefabName = Config.Bind<string>("General", "PrefabName", "Jim", "The prefab to use the repair ai with (repair structures)");
-            TamingItemList = Config.Bind<string>("General", "TamingItemList", "Dandelion", "Comma separated list if items used to tame villager");
-            HungryItemList = Config.Bind<string>("General", "PostTameConsumables", "QueensJam,Raspberry,Honey,Blueberries,Resin,Dandelion", "Comma separated list if items Brutes eat when hungry");
-            FeedDuration = Config.Bind<int>("General", "Greyling_FeedDuration", 500, "Time before getting hungry after consuming one item");
-            PreTameFeedDuration = Config.Bind<int>("General", "PreTameFeedDuration", 100, "Time before getting hungry after consuming one item during taming");
-            PostTameFeedDuration = Config.Bind<int>("General", "PostTameFeedDuration", 1000, "Time before getting hungry after consuming one item when tame");
-            TamingTime = Config.Bind<int>("General", "TamingTime", 1000, "Total time it takes to tame a Brute");
-            TimeLimitOnAssignment = Config.Bind<int>("General", "TimeLimitOnAssignment", 30, "How long before moving on to next assignment");
-            IncludedContainersList = Config.Bind<string>("General", "IncludedContainersList", "piece_chest_wood", "Comma separated list of container piece names to be searchable by Greylings");
+            _serverConfigLocked = config("General", "Lock Configuration", false, "Lock Configuration");
+            BrutePrefabName = config("General", "PrefabName", "Jim", "The prefab to use the repair ai with (repair structures)");
+            TamingItemList = config("General", "TamingItemList", "Dandelion", "Comma separated list if items used to tame villager");
+            HungryItemList = config("General", "PostTameConsumables", "QueensJam,Raspberry,Honey,Blueberries,Resin,Dandelion", "Comma separated list if items Brutes eat when hungry");
+            FeedDuration = config("General", "Greyling_FeedDuration", 500, "Time before getting hungry after consuming one item");
+            PreTameFeedDuration = config("General", "PreTameFeedDuration", 100, "Time before getting hungry after consuming one item during taming");
+            PostTameFeedDuration = config("General", "PostTameFeedDuration", 1000, "Time before getting hungry after consuming one item when tame");
+            TamingTime = config("General", "TamingTime", 1000, "Total time it takes to tame a Brute");
+            TimeLimitOnAssignment = config("General", "TimeLimitOnAssignment", 30, "How long before moving on to next assignment");
+            IncludedContainersList = config("General", "IncludedContainersList", "piece_chest_wood", "Comma separated list of container piece names to be searchable by Greylings");
             PreTameConsumables = TamingItemList.Value.Replace(" ", "").Split(',', ';');
             PostTameConsumables = HungryItemList.Value.Replace(" ", "").Split(',', ';');
-            Awareness = Config.Bind<int>("General", "Awareness", 6, "General awareness, used to calculate search ranges and ability to detect enemies");
-            Agressiveness = Config.Bind<int>("General", "Agressiveness", 8, "Agressivness determines how to behave when fighting and when to give up and flee");
-            Mobility = Config.Bind<int>("General", "Mobility", 10, "Mobility is used to determine how often and how far the mob moves");
-            Intelligence = Config.Bind<int>("General", "Intelligence", 5, "General intelligence, how much the mob can remember");
-
+            Awareness = config("General", "Awareness", 6, "General awareness, used to calculate search ranges and ability to detect enemies");
+            Agressiveness = config("General", "Agressiveness", 8, "Agressivness determines how to behave when fighting and when to give up and flee");
+            Mobility = config("General", "Mobility", 10, "Mobility is used to determine how often and how far the mob moves");
+            Intelligence = config("General", "Intelligence", 5, "General intelligence, how much the mob can remember");
+            configSync.AddLockingConfigEntry(_serverConfigLocked);
         }
     }
 }
